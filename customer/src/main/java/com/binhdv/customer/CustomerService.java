1e9b282f8d22d7f8e208e48a700e8da919405106
binhdv <vanbinhdoan971@gmail.com>
2022-06-09 14:15:27 +0700

    demo api, docker compose file

---
+++ customer/src/main/java/com/binhdv/customer/CustomerService.java
@@ -0,0 +1,16 @@
+package com.binhdv.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .build();
+    }
+
+}
